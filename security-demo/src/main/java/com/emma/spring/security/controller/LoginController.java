066d6bd9ece667aa4e2fa80ce8265fdbac4b394e
刘健 <luckysky1013@163.com>
2019-04-26 15:59:48 +0800

    xml配置方式

---
+++ security-demo/src/main/java/com/emma/spring/security/controller/LoginController.java
@@ -0,0 +1,42 @@
+package com.emma.spring.security.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @author liujian
+ * @date 2019/4/26
+ */
+@Controller
+@RequestMapping("/")
+public class LoginController {
+
+	/**
+	 * 返回 hello 视图
+	 * @return
+	 */
+	@RequestMapping(value = { "/", "/welcome**" }, method = RequestMethod.GET)
+	public ModelAndView welcomePage() {
+		ModelAndView model = new ModelAndView();
+		model.addObject("title", "Spring Security Hello World");
+		model.addObject("message", "This is welcome page!");
+		model.setViewName("hello");
+		return model;
+	}
+
+	/**
+	 * 返回 admin 视图
+	 * @return
+	 */
+	@RequestMapping(value = "/admin**", method = RequestMethod.GET)
+	public ModelAndView adminPage() {
+		ModelAndView model = new ModelAndView();
+		model.addObject("title", "Spring Security Hello World");
+		model.addObject("message", "This is protected page!");
+		model.setViewName("admin");
+		return model;
+	}
+
+}
---
+++ security-demo/src/main/webapp/WEB-INF/pages/hello.jsp
@@ -0,0 +1,9 @@
+<%@ page language="java" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+    <meta charset="utf-8">
+</head>
+<body><h1>标题：${title}</h1>
+<h1>消息：${message}</h1></body>
+</html>
